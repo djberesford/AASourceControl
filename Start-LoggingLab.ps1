90a8b436d11d004fc3c5111ccd28eeb2caa3f39b
djberesford <33470973+djberesford@users.noreply.github.com>
2017-11-08 15:11:53 +1300

    Created Start-LoggingLab.ps1 in AzureAutomation.

---
+++ Start-LoggingLab.ps1
@@ -0,0 +1,14 @@
+workflow Start-LoggingLab
+{
+    Write-output "This is an Output Line"
+    
+    Write-debug "This is a debug line"
+    
+        Write-verbose "This is a verbose line"
+    
+        Write-progress "This is a progress line"
+    
+    Write-warning "This is a warning line"
+    
+    Write-Error "This is an Error Line"
+}
